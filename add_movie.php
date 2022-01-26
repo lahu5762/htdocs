e5d3cea8d7c74ac48518b03797287d85a2c46801
lahu5762 <91313177+lahu5762@users.noreply.github.com>
2022-01-26 12:30:15 +0100

    updated sql insert statement

--- add_movie.php
+++ add_movie.php
@@ -2,16 +2,16 @@
     // connect to database
     include 'db.php';
 
-    // insert into movies (...) values (name, year, rating, (gID where gGenre=input))
+    // insert into movies (...) values (name, year, rating, (gID where gGenre like input))
     $sql_insert = "INSERT INTO movies(mName, mYear, mRating, mGenreID) 
     VALUES ('" . $_POST['movie_name'] . "', '" . $_POST['movie_year'] . "', 
-    '" . $_POST['movie_rating'] . "', (SELECT gID FROM genres WHERE gGenre='" 
+    '" . $_POST['movie_rating'] . "', (SELECT gID FROM genres WHERE gGenre LIKE '" 
     . $_POST['movie_genre'] . "'))";
     
     if (mysqli_query($link, $sql_insert)) {
         echo "New record created successfully.";
     } else {
-        echo 'Error: ' . $sql_insert . '<br>' . mysqli_error($link);
+        echo "Something went wrong.";
     }
 ?>
 <form action="index.php" method="POST">
